1c8290c7e44281277f94f95a3bc25aab52ba9db8
Enrico <vincentwelles@protonmail.com>
2025-11-18 20:44:53 -0300

    Criando o modelo do slideshow

--- src/js/gerson.js
+++ src/js/gerson.js
@@ -1,4 +1,16 @@
-let FotoGerson = [
-    '../assets/img/gerson/rutenio.jpg'
+let information = [
+    "Nome completo: Gerson Altamir",
+    "Data de nascimento: 10/10/1990",
+    "Cidade: São Paulo",
+    "Profissão: Engenheiro de Software",
+    "Formação: Bacharel em Engenharia de Software pela USP",
+    "Soft Skills: Comunicação, Trabalho em equipe, Resolução de problemas",
+    "Hobbies: Leitura, Ciclismo, Programação"
 ]
 
+let i = 0;
+let time = 5000
+
+function SlideShow() {
+    
+}
