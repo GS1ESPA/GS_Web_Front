ae372322cad8491814bd5d888882ec1a0aced80d
Enrico <vincentwelles@protonmail.com>
2025-11-18 20:51:47 -0300

    Primeiro slideshow funcionando

--- src/js/gerson.js
+++ src/js/gerson.js
@@ -12,5 +12,12 @@ let i = 0;
 let time = 5000
 
 function SlideShow() {
-    
+    document.getElementById("nome").innerText = information[i];
+    i++;
+    if (i >= information.length) {
+        i = 0;
+    }
+    setTimeout(SlideShow, time);
 }
+
+SlideShow();
